5f3dab3b035a6a372062d689645e45bceb99a4a9
WI91 <elammar.will@gmail.com>
2025-05-20 10:35:18 +0100

    Feat: Initial Commit

---
+++ src/main/java/com/example/JV_Exhibition_Curation/JvExhibitionCurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.JV_Exhibition_Curation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvExhibitionCurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvExhibitionCurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JV_Exhibition_Curation/JvExhibitionCurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JV_Exhibition_Curation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvExhibitionCurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
