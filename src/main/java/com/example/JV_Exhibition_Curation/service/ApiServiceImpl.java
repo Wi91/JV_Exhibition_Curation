71ae9fc7a5ce6c8be2d22ae0c4dab4b287b88979
WI91 <elammar.will@gmail.com>
2025-06-02 16:47:20 +0100

    Feat: Add get request method

--- src/main/java/com/example/JV_Exhibition_Curation/service/ApiServiceImpl.java
+++ src/main/java/com/example/JV_Exhibition_Curation/service/ApiServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.JV_Exhibition_Curation.service;
 
 import com.example.JV_Exhibition_Curation.model.Artwork;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +18,63 @@ public class ApiServiceImpl implements ApiService{
 
 
     private static final String CHICAGO_ARTWORK_ALL_ARTWORKS_URL ="https://api.artic.edu/api/v1/artworks?limit=10&page=";
+    private static final String CHICAGO_IMAGE_BASE_URL = "https://www.artic.edu/iiif/2";
 
 
     @Override
     public List<Artwork> getAllHomeArtworks(Integer page) {
 
         ArrayList<Artwork> artworks = new ArrayList<>();
-        return List.of();
+        ArrayList<Artwork> artworksChicago = getAllHomeChicagoArtworks(page);
+        List<Artwork> list = new ArrayList<>(artworksChicago);
+
+        return list;
+    }
+
+    private ArrayList<Artwork> getAllHomeChicagoArtworks(Integer page) {
+
+        ArrayList<Artwork> chicagoArtworks = new ArrayList<>();
+
+        String url = CHICAGO_ARTWORK_ALL_ARTWORKS_URL + page;
+        JsonNode results = SendGETRequest(url);
+
+        JsonNode data = results.findPath("data");
+        for(JsonNode node : data) {
+
+
+            Artwork art = Artwork.builder()
+                    .apiId(node.path("id").asLong(0))
+                    .title(node.path("title").asText("Unknown"))
+                    .altText(node.path("thumbnail").path("alt_text").asText("None Provided"))
+                    .apiOrigin("Chicago Institute")
+                    .artistName(node.path("artist_title").asText("Unknown"))
+                    .description(node.path("description").asText("No Description Provided"))
+                    .imageUrl(CHICAGO_IMAGE_BASE_URL + node.path("image_id").asText("No Image") + "/full/843,/0/default.jpg")
+                    .build();
+
+            chicagoArtworks.add(art);
+        }
+
+        return chicagoArtworks;
+    }
+
+    private JsonNode SendGETRequest(String url) {
+
+        HttpRequest chiSearchRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newBuilder().build();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+           HttpResponse<String> response = client.send(chiSearchRequest, HttpResponse.BodyHandlers.ofString());
+           return mapper.readTree(response.body());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
