28bb9116af17215b1cca518163ce1f53c39db3ea
WI91 <elammar.will@gmail.com>
2025-06-03 15:52:21 +0100

    Feat: Update Artwork Model to avoid null values

--- src/main/java/com/example/JV_Exhibition_Curation/model/Artwork.java
+++ src/main/java/com/example/JV_Exhibition_Curation/model/Artwork.java
@@ -1,7 +1,9 @@
 package com.example.JV_Exhibition_Curation.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 @AllArgsConstructor
@@ -17,12 +19,20 @@ public class Artwork {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     Long id;
+    @NotNull(message = "Api Id cannot be null")
+    @Range(min = 0, message = "Api Id must be 0 or above")
     Long apiId;
+    @NotNull(message = "Title cannot be null")
     String title;
+    @NotNull(message = "Description cannot be null")
     String description;
+    @NotNull(message = "Alt Text cannot be null")
     String altText;
+    @NotNull(message = "Artist name cannot be null")
     String artistName;
+    @NotNull(message = "Image Url cannot be null")
     String imageUrl;
+    @NotNull(message = "Api origin cannot be null")
     String apiOrigin;
 
 }
