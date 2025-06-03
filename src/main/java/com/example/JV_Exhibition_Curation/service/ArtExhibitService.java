f07dd0a201763ffd5d2cea14c90043ab31e7b9d1
WI91 <elammar.will@gmail.com>
2025-06-03 15:53:30 +0100

    Feat: Add methods to ArtExhibitService

--- src/main/java/com/example/JV_Exhibition_Curation/service/ArtExhibitService.java
+++ src/main/java/com/example/JV_Exhibition_Curation/service/ArtExhibitService.java
@@ -1,6 +1,9 @@
 package com.example.JV_Exhibition_Curation.service;
 
+import com.example.JV_Exhibition_Curation.dto.SavedArtworksDTO;
 import com.example.JV_Exhibition_Curation.model.Artwork;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.List;
 
@@ -8,4 +11,9 @@ public interface ArtExhibitService {
     List<Artwork> getAllHomeArtworks(Integer page);
 
 
+    List<Artwork> getSearchResults(String query, Integer page);
+
+    HttpStatusCode addArtworkToExhibition(@Valid SavedArtworksDTO savedArtworksDTO, Long exhibitionID);
+
+    void removeArtworkFromExhibition(Long exhibitionId, @Valid SavedArtworksDTO savedArtworksDTO);
 }
