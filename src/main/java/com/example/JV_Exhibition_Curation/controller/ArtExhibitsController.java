feceb638b06a2bb4bd48cb0c0fd38e47870ca44f
WI91 <elammar.will@gmail.com>
2025-06-03 15:53:57 +0100

    Feat: Add mappings to controller

--- src/main/java/com/example/JV_Exhibition_Curation/controller/ArtExhibitsController.java
+++ src/main/java/com/example/JV_Exhibition_Curation/controller/ArtExhibitsController.java
@@ -1,14 +1,13 @@
 package com.example.JV_Exhibition_Curation.controller;
 
+import com.example.JV_Exhibition_Curation.dto.SavedArtworksDTO;
 import com.example.JV_Exhibition_Curation.model.Artwork;
 import com.example.JV_Exhibition_Curation.service.ArtExhibitService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +21,24 @@ public class ArtExhibitsController {
     @GetMapping("/artwork/home")
     public ResponseEntity<List<Artwork>> getAllHomeArtworks(@RequestParam Integer page){
         return new ResponseEntity<>(artExhibitService.getAllHomeArtworks(page), HttpStatus.OK);
+    }
+
+    //Search artworks
+    @GetMapping("/artwork/home/search")
+    public ResponseEntity<List<Artwork>> searchArtworks(@RequestParam String query, @RequestParam Integer page){
+        return new ResponseEntity<>(artExhibitService.getSearchResults(query, page), HttpStatus.OK);
+    }
+
+    //Create exhibition
+    @PostMapping("/exhibitions/{exhibitionId}")
+    public ResponseEntity<List<Artwork>> addArtworkToExhibition (@RequestBody @Valid SavedArtworksDTO savedArtworksDTO, @PathVariable Long exhibitionId){
+        return new ResponseEntity<>(artExhibitService.addArtworkToExhibition(savedArtworksDTO, exhibitionId));
+    }
+
+    @DeleteMapping("/exhibitions/{exhibitionId}/{artworkId}")
+    public ResponseEntity<Artwork> removeArtworkFromExhibition (@PathVariable Long exhibitionId, @RequestBody @Valid SavedArtworksDTO savedArtworksDTO){
+        artExhibitService.removeArtworkFromExhibition(exhibitionId, savedArtworksDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
