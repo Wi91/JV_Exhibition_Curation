fbfa5d5ac3c43ba7f8f743bb1e10586b73a04ec8
WI91 <elammar.will@gmail.com>
2025-06-03 15:53:01 +0100

    Feat: Add override methods to ArtExhibitServiceImpl

--- src/main/java/com/example/JV_Exhibition_Curation/service/ArtExhibitServiceImpl.java
+++ src/main/java/com/example/JV_Exhibition_Curation/service/ArtExhibitServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.JV_Exhibition_Curation.service;
 
+import com.example.JV_Exhibition_Curation.dto.SavedArtworksDTO;
 import com.example.JV_Exhibition_Curation.model.Artwork;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +16,27 @@ public class ArtExhibitServiceImpl implements ArtExhibitService{
 
     @Override
     public List<Artwork> getAllHomeArtworks(Integer page) {
+        if(page == null || page < 1){
+            //Add exception
+        }
         return apiService.getAllHomeArtworks(page);
+    }
+
+    @Override
+    public List<Artwork> getSearchResults(String query, Integer page) {
+        if(page == null || page < 1){
+            //Add exception
+        }
+        return List.of();
+    }
+
+    @Override
+    public HttpStatusCode addArtworkToExhibition(SavedArtworksDTO savedArtworksDTO, Long exhibitionID) {
+        return null;
+    }
+
+    @Override
+    public void removeArtworkFromExhibition(Long exhibitionId, SavedArtworksDTO savedArtworksDTO) {
+
     }
 }
