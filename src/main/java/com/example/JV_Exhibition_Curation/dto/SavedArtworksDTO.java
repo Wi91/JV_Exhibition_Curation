f6f3b5662c15e6d0957b6c9b65b4fa2d6f3680d0
WI91 <elammar.will@gmail.com>
2025-06-04 16:16:56 +0100

    Feat: Update DTO annotations

--- src/main/java/com/example/JV_Exhibition_Curation/dto/SavedArtworksDTO.java
+++ src/main/java/com/example/JV_Exhibition_Curation/dto/SavedArtworksDTO.java
@@ -2,7 +2,16 @@ package com.example.JV_Exhibition_Curation.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 
 public class SavedArtworksDTO {
     @NotNull(message = "ArtworkId cannot be null")
