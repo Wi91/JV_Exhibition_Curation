a9645bbda35ce4247d7d23ae09f9d95315a497ef
WI91 <elammar.will@gmail.com>
2025-06-04 16:18:42 +0100

    Feat: Add mappings for creating exhibition, deleting exhibition

--- src/main/java/com/example/JV_Exhibition_Curation/controller/ArtExhibitsController.java
+++ src/main/java/com/example/JV_Exhibition_Curation/controller/ArtExhibitsController.java
@@ -2,6 +2,7 @@ package com.example.JV_Exhibition_Curation.controller;
 
 import com.example.JV_Exhibition_Curation.dto.SavedArtworksDTO;
 import com.example.JV_Exhibition_Curation.model.Artwork;
+import com.example.JV_Exhibition_Curation.model.Exhibition;
 import com.example.JV_Exhibition_Curation.service.ArtExhibitService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,23 +24,47 @@ public class ArtExhibitsController {
         return new ResponseEntity<>(artExhibitService.getAllHomeArtworks(page), HttpStatus.OK);
     }
 
-    //Search artworks
+    //Search All Artworks
     @GetMapping("/artwork/home/search")
     public ResponseEntity<List<Artwork>> searchArtworks(@RequestParam String query, @RequestParam Integer page){
         return new ResponseEntity<>(artExhibitService.getSearchResults(query, page), HttpStatus.OK);
     }
 
-    //Create exhibition
+    //Add Artwork To Exhibition
     @PostMapping("/exhibitions/{exhibitionId}")
     public ResponseEntity<List<Artwork>> addArtworkToExhibition (@RequestBody @Valid SavedArtworksDTO savedArtworksDTO, @PathVariable Long exhibitionId){
         return new ResponseEntity<>(artExhibitService.addArtworkToExhibition(savedArtworksDTO, exhibitionId));
     }
 
+    //Delete Artwork From Exhibition
     @DeleteMapping("/exhibitions/{exhibitionId}/{artworkId}")
     public ResponseEntity<Artwork> removeArtworkFromExhibition (@PathVariable Long exhibitionId, @RequestBody @Valid SavedArtworksDTO savedArtworksDTO){
         artExhibitService.removeArtworkFromExhibition(exhibitionId, savedArtworksDTO);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    //Create Exhibition
+    @PostMapping("/exhibitions")
+    public ResponseEntity<Exhibition> createNewExhibition (){
+        return new ResponseEntity<>(artExhibitService.createNewExhibition(), HttpStatus.CREATED);
+    }
+
+    //Delete Exhibition
+    @DeleteMapping("/exhibitions/{id}")
+    public ResponseEntity<Exhibition> deleteExhibition (@PathVariable Long exhibitionId){
+        artExhibitService.deleteExhibition(exhibitionId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    //Retrieve all Exhibitions
+    @GetMapping("/exhibitions")
+    public ResponseEntity<List<Exhibition>> getAllExhibitions () {
+        return new ResponseEntity<>(artExhibitService.getAllExhibitions(), HttpStatus.OK);
+    }
+
+    @GetMapping("/exhibitions/{id}")
+    public ResponseEntity<Exhibition> getExhibition (@PathVariable Long exhibitionId) {
+        return new ResponseEntity<>(artExhibitService.getExhibition(exhibitionId), HttpStatus.OK);
+    }
 
 }
