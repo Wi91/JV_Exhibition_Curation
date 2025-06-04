c5a826c09319a91551f629eb58c727b689e54d81
WI91 <elammar.will@gmail.com>
2025-06-04 16:47:07 +0100

    Feat: Add exhibition repository

---
+++ src/main/java/com/example/JV_Exhibition_Curation/repository/ExhibitionRepository.java
@@ -0,0 +1,7 @@
+package com.example.JV_Exhibition_Curation.repository;
+
+import com.example.JV_Exhibition_Curation.model.Exhibition;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExhibitionRepository extends CrudRepository<Exhibition, Long> {
+}
